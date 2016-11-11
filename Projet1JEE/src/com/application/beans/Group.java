3b4771ce0f3a3b299e40a847c53f9cea15f32d1d
hamoud manlid <abdoulrahiim@gmail.com>
2016-11-11 13:22:59 +0100

    ajout de bean group

---
+++ Projet1JEE/src/com/application/beans/Group.java
@@ -0,0 +1,41 @@
+package com.application.beans;
+
+/**
+ * 
+ * @author Houmed, Abdoulrahim
+ *
+ */
+public class Group {
+	private int idGroup;
+	private String name;
+	/**
+	 * Constructor
+	 */
+	public Group(){
+		
+	}
+	/**
+	 * @return idGroup
+	 */
+	public int getIdGroup() {
+		return idGroup;
+	}
+	/**
+	 * @param idGroup
+	 */
+	public void setIdGroup(int idGroup) {
+		this.idGroup = idGroup;
+	}
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
